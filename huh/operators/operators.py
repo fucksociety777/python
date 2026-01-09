f384d7ca5c6edf0ae96d8cc58245adec0303d9d4
Anmol <anmol@Anmols-Mac-mini.local>
2026-01-09 18:55:34 +0530

    shkdjshd

--- huh/operators/operators.py
+++ huh/operators/operators.py
@@ -3,10 +3,10 @@ a = 30
 b = 2
 
 # Arithmetic Operator
-print("a + b = ", a + b)
-print("a - b = ", a - b)
-print("a * b = ", a * b)
-print("a / b = ", a / b)
-print("a % b = ", a % b)
-print("a // b = ", a // b)
-print("a ** b = ", a ** b)
+print("a + b = ", a + b) #addition
+print("a - b = ", a - b) #subtraction
+print("a * b = ", a * b) #multiplication
+print("a / b = ", a / b) #division
+print("a % b = ", a % b) #Modulus
+print("a // b = ", a // b) #Floor division
+print("a ** b = ", a ** b) #Exponentiation
