8f610a128906117dd01d434d1fc77a5a51285cc3
Anmol <anmol@Anmols-Mac-mini.local>
2026-01-09 18:53:27 +0530

    first commit

---
+++ huh/operators/operators.py
@@ -0,0 +1,12 @@
+a = 30 
+
+b = 2
+
+# Arithmetic Operator
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a // b = ", a // b)
+print("a ** b = ", a ** b)
---
+++ huh/typecasting/01_typecasting.py
@@ -0,0 +1,16 @@
+a = 53
+b = "43"
+d = 453
+
+print(type(a))
+print(type(b))
+
+# convert b to an integer
+c = int(b)
+print(c)
+print(type(c))
+
+
+e = str(d)
+print(e)
+print(type(e))
