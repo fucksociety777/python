e2f6685a0dca77596f12d4c1bf74e694d5a58c76
Anmol <anmol@Anmols-Mac-mini.local>
2026-01-09 19:19:48 +0530

    operators

---
+++ huh/operators/conditional_operators.py
@@ -0,0 +1,10 @@
+a = 30
+b = 2
+# Conditional Operators
+print(a>4)
+print(a<4)
+print(a<=4)
+print(a>=4)
+print(a==4) # Is a equal to 4?
+print(a==30) # Is a equal to 34? 
+print(a!=30)  # Is a not equal to 34?
