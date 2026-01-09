5cbb5a96d19521bc95308b848cc818d212e049f6
Anmol <anmol@Anmols-Mac-mini.local>
2026-01-09 19:12:44 +0530

    operators

--- huh/operators/operators.py
+++ huh/operators/operators.py
@@ -10,3 +10,12 @@ print("a / b = ", a / b) #division
 print("a % b = ", a % b) #Modulus
 print("a // b = ", a // b) #Floor division
 print("a ** b = ", a ** b) #Exponentiation
+
+# Conditional Operators
+print(a>4)
+print(a<4)
+print(a<=4)
+print(a>=4)
+print(a==4) # Is a equal to 4?
+print(a==30) # Is a equal to 34? 
+print(a!=30)  # Is a not equal to 34?
