f0ebba099481aa9bd5b12905ae41764d049d3b30
Anmol <anmol@Anmols-Mac-mini.local>
2026-01-09 19:21:20 +0530

    operators

---
+++ huh/operators/Logical_operators.py
@@ -0,0 +1,8 @@
+
+#Logical Operators
+c = True
+d = False
+print(c and c)
+print(c and d)
+print(d and c)
+print(c and d)
--- huh/operators/arithemetic_operators.py
+++ huh/operators/arithemetic_operators.py
@@ -11,11 +11,5 @@ print("a % b = ", a % b) #Modulus
 print("a // b = ", a // b) #Floor division
 print("a ** b = ", a ** b) #Exponentiation
 
-# Conditional Operators
-print(a>4)
-print(a<4)
-print(a<=4)
-print(a>=4)
-print(a==4) # Is a equal to 4?
-print(a==30) # Is a equal to 34? 
-print(a!=30)  # Is a not equal to 34?
+
+
