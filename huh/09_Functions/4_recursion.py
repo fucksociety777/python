4d64e424e6356eece8db8dccac746aa1b94a3c33
fucksociety777 <alfennzoremote@gmail.com>
2026-01-16 11:48:36 +0000

    huh

--- huh/09_Functions/4_recursion.py
+++ huh/09_Functions/4_recursion.py
@@ -1,2 +1,20 @@
-# fibonacci series 0 1 1 2 3 5 8 13.  (just ADD PREVIOUS 2 NUMBERS)
+'''
+fibonacci series 0 1 1 2 3 5 8 13.  (just ADD PREVIOUS 2 NUMBERS)
+                 0 1 2 3 4 5 6 7......
 
+                fib(0) = 0
+                fib(1) = 1
+                fib(2) = fib(0) + fib(1)
+                fib(3) = fib(1) + fib(2)
+                fib(4) = fib(2) + fib(3)
+                fib(n) = fib(n-2) + fib(n-1)
+
+'''
+def fib(n):
+    #Base case for recursion
+    if(n == 0 or n == 1):
+        return n
+    
+    return fib(n-2) + fib(n-1)
+
+print(fib(6))
---
+++ huh/09_Functions/5_modules.py
@@ -0,0 +1,15 @@
+# Two types of modules in python:
+# - BUilt in Modules
+# - External Modules
+# List of all the built in Modules: https://docs.python.org/3/py-modindex.html
+
+import math
+import os
+import mymodule
+import requests        
+
+os.abort
+print(math.sqrt(16))
+mymodule.hello()
+r = requests.get("https://www.alfennzo.com")
+print(r.text)
