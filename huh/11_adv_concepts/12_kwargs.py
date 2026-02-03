83c4cb716abd7807ea549b293fcbdbe50597b332
fucksociety777 <alfennzoremote@gmail.com>
2026-02-03 10:21:45 +0000

    huh

--- huh/11_adv_concepts/12_kwargs.py
+++ huh/11_adv_concepts/12_kwargs.py
@@ -1,6 +1,7 @@
 def marks(**kwargs):
-    # kwargs is a dictionary with all the key value pairs which were passed to marks
     for item in kwargs.keys():
         print(f"The marks of {item} is {kwargs[item]}")
         
 marks(goku=20, gohan=15, rikin=11, vegeta=19)
+
+# kwargs is a dictionary with all the key value pairs which were passed to marks
